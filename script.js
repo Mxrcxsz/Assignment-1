9cefcb844893a7a67a136a731157e9947fbf78a6
Mxrcxsz <s10208495@connect.np.edu.sg>
2020-11-24 14:36:18 +0800

    Fixed Responsive Error

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ var y = document.getElementById("flex-container1");
 var z = document.getElementById("flex-container2");
 var yc = document.getElementById("snkrs");
 var zc = document.getElementById("apprl");
+var ys = document.getElementById("s-icon");
+var zs = document.getElementById("a-icon");
 var check = true
 
 z.style.display = "none";
@@ -29,8 +31,10 @@ function showApprl(){
     {
         zc.style.backgroundColor = "black";
         zc.style.color = "white";
+        zs.src = "img/shirt.png"
         yc.style.backgroundColor = "white";
         yc.style.color = "black";
+        ys.src = "img/sneakers (1).png"
         check = false;
     }
 }
@@ -42,8 +46,10 @@ function showSnkrs(){
     {
         yc.style.backgroundColor = "black";
         yc.style.color = "white";
+        ys.src = "img/sneakers.png"
         zc.style.backgroundColor = "white";
         zc.style.color = "black";
+        zs.src = "img/shirt (1).png"
         check = true;
     }
 }
